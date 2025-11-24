e8dd3beb1c626d318d037a1d1cd595d75e0c4612
up2040449 <up2040449@myport.ac.uk>
2025-11-24 17:04:55 +0000

    Updated animations

--- src/intro.jsx
+++ src/intro.jsx
@@ -18,72 +18,18 @@ function Intro(){
         gsap.fromTo(".intro-img", 
             {
                 scale: 1,
-                xPercent: -50,
-                yPercent: -50
             },
             {
                 scale: 0.85,
-                xPercent: -50,
-                yPercent: -50,
                 ease: "power1.inOut",
                 scrollTrigger: {
                     immediateRender: false,
                     trigger: locationsRef.current,
-                    start: "top-=150px bottom",
-                    end: "top-=150px 15%",
+                    start: "top bottom",
+                    end: "top 15%",
                     scrub: 0.5
                 }
         });
-
-        const tlNZ = gsap.timeline(
-        {
-            scrollTrigger: {
-                pinSpacing: false,
-                trigger: '.intro-img-wrapper',
-                start: "top top",
-                end: "bottom top",
-                scrub: 1.5,
-                ease: "power1.inOut",
-                pin: true,
-                onLeave: () => toggleTextVisible(tv => !tv),
-                onEnterBack: () => toggleTextVisible(tv => !tv)
-            }
-        }
-        )
-        
-        tlNZ.fromTo(".intro-img",
-            {
-                scale: 0.3,
-                rotateZ: -56,
-                rotateX: 100,
-                xPercent: -50,
-                yPercent: -50,
-                filter: 'grayscale(1) brightness(0.6)'
-            },
-            {
-                duration: 0.9,
-                rotateZ: 0,
-                rotateX: 0,
-                scale: 1,
-                xPercent: -50,
-                yPercent: -50,
-                filter: 'grayscale(0) brightness(1.05)'
-            }
-        )
-            .fromTo(".intro-img-text", 
-                {
-                    scale: 1,
-                    xPercent: -50,
-                    yPercent: -50
-                },
-                {
-                    duration: 1,
-                    scale: 30,
-                    xPercent: -50,
-                    yPercent: -50,
-                    ease: "power2.in"
-                }
-                ,"<");
 
         const elems = gsap.utils.toArray(".location-text");
         elems.forEach((e) => {
@@ -152,11 +98,9 @@ function Intro(){
     
     return(
         <>
-            <div className='gradient-bg'>
-                <div className="intro-img-wrapper">
-                    <img src={temple} alt="Angkor Wat" className="intro-img" />
-                    <div className="intro-img-text" style={{ display: !textVisible ? "none" : "block" }}>2025<br/>TRAVEL</div>
-                </div>
+            <div className="intro-img-wrapper">
+                <img src={temple} alt="Angkor Wat" className="intro-img" />
+                <div className="intro-img-text" style={{ display: !textVisible ? "none" : "block" }}>2025</div>
             </div>
             <div className="locations-wrapper" ref={locationsRef}>
                 {locationList.map((value,index) => 
